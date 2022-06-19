c65a145a4225efe69d5d58b5bc396ccdcbed3fb5
karenlaw-0316 <lawhiuyankarenlaw@gmail.com>
2022-06-19 16:01:47 +0800

    remove console log and unnecessary comments

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -69,7 +69,6 @@ const Homepage = () => {
 	];
 	const [cm, setCM] = React.useState("");
 	const [comments, setComments] = React.useState(defaultComments);
-	console.log(comments);
 
 	const navItems = [
 		{ icon: <HomeOutlinedIcon />, text: "Homepage" },
@@ -108,8 +107,6 @@ const Homepage = () => {
 	];
 
 	const onCommentPost = () => {
-		console.log("try");
-		console.log(cm);
 		let newComments = [...comments];
 		newComments.push({
 			avatar: UserAvatar,
@@ -118,7 +115,6 @@ const Homepage = () => {
 		});
 		setComments(newComments);
 		setCM("");
-		// console.log(comments);
 	};
 
 	return (
@@ -129,10 +125,6 @@ const Homepage = () => {
 				sx={{
 					backgroundColor: "white",
 					boxShadow: "0px 4px 20px 2px rgba(236, 236, 236, 0.42)",
-					// alignItems: "center",
-					// display: "flex",
-					// justifyContent: "space-between",
-					// padding: 1,
 				}}
 			>
 				<div
@@ -165,7 +157,6 @@ const Homepage = () => {
 							id="outlined-search"
 							label="Search"
 							type="search"
-							// variant="filled"
 							size="small"
 						/>
 						<Avatar alt="user" src={UserAvatar} />
@@ -395,139 +386,11 @@ const Homepage = () => {
 												</Box>
 											);
 										})}
-										{/* <Box
-											sx={{
-												backgroundColor: "white",
-												padding: 3,
-												borderRadius: 2,
-											}}
-										>
-											<Stack direction="row" spacing={2}>
-												<Avatar
-													alt="user"
-													src={UserAvatar3}
-													sx={{ width: 40, height: 40 }}
-												/>
-												<Stack spacing={1}>
-													<Typography
-														sx={{
-															fontSize: "16px",
-															color: "#0B5286",
-															fontWeight: 700,
-														}}
-													>
-														Joyce N.
-													</Typography>
-
-													<Typography sx={{ fontSize: "14px" }}>
-														Lorem ipsum dolor sit amet, consectetur adipiscing
-														elit. Suspendisse malesuada lacus ex, sit amet
-														blandit leo lobortis eget.
-													</Typography>
-												</Stack>
-											</Stack>
-										</Box>
-										<Box
-											sx={{
-												backgroundColor: "white",
-												padding: 3,
-												borderRadius: 2,
-											}}
-										>
-											<Stack direction="row" spacing={2}>
-												<Avatar
-													alt="user"
-													src={UserAvatar4}
-													sx={{ width: 40, height: 40 }}
-												/>
-												<Stack spacing={1}>
-													<Typography
-														sx={{
-															fontSize: "16px",
-															color: "#0B5286",
-															fontWeight: 700,
-														}}
-													>
-														Anna A.
-													</Typography>
-
-													<Typography sx={{ fontSize: "14px" }}>
-														Lorem ipsum dolor sit amet, consectetur adipiscing
-														elit.
-													</Typography>
-												</Stack>
-											</Stack>
-										</Box>
-										<Box
-											sx={{
-												backgroundColor: "white",
-												padding: 3,
-												borderRadius: 2,
-											}}
-										>
-											<Stack direction="row" spacing={2}>
-												<Avatar
-													alt="user"
-													src={UserAvatar3}
-													sx={{ width: 40, height: 40 }}
-												/>
-												<Stack spacing={1}>
-													<Typography
-														sx={{
-															fontSize: "16px",
-															color: "#0B5286",
-															fontWeight: 700,
-														}}
-													>
-														Joyce N.
-													</Typography>
-
-													<Typography sx={{ fontSize: "14px" }}>
-														Lorem ipsum dolor sit amet, consectetur adipiscing
-														elit. Suspendisse malesuada lacus ex, sit amet
-														blandit leo lobortis eget.
-													</Typography>
-												</Stack>
-											</Stack>
-										</Box>
-										<Box
-											sx={{
-												backgroundColor: "white",
-												padding: 3,
-												borderRadius: 2,
-											}}
-										>
-											<Stack direction="row" spacing={2}>
-												<Avatar
-													alt="user"
-													src={UserAvatar3}
-													sx={{ width: 40, height: 40 }}
-												/>
-												<Stack spacing={1}>
-													<Typography
-														sx={{
-															fontSize: "16px",
-															color: "#0B5286",
-															fontWeight: 700,
-														}}
-													>
-														Joyce N.
-													</Typography>
-
-													<Typography sx={{ fontSize: "14px" }}>
-														Lorem ipsum dolor sit amet, consectetur adipiscing
-														elit. Suspendisse malesuada lacus ex, sit amet
-														blandit leo lobortis eget.
-													</Typography>
-												</Stack>
-											</Stack>
-										</Box> */}
 									</Stack>
 								</Box>
 								<Box
 									sx={{
 										backgroundColor: "white",
-										// height: "100px",
 										padding: "20px",
 										boxShadow: "0px -30px 20px -20px #0000000D",
 									}}
@@ -551,12 +414,10 @@ const Homepage = () => {
 										</Stack>
 										<TextField
 											id="outlined-multiline-static"
-											// label="Multiline"
 											multiline
 											rows={3}
 											value={cm}
 											onChange={(e) => setCM(e.target.value)}
-											// defaultValue="Default Value"
 										/>
 									</Stack>
 								</Box>
@@ -632,19 +493,6 @@ const Homepage = () => {
 					</Stack>
 				</Grid>
 			</Grid>
-			{/* <header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header> */}
 		</Box>
 	);
 };
